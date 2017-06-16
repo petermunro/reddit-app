8f7a1870fa8f062e564779a8ef12178f7136eb56
Peter Munro <peter@petermunro.org>
2017-06-16 16:27:02 +0100

    Initial import

---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_REDDIT = 'SELECT_REDDIT'
+export const INVALIDATE_REDDIT = 'INVALIDATE_REDDIT'
+
+export const selectReddit = reddit => ({
+  type: SELECT_REDDIT,
+  reddit
+})
+
+export const invalidateReddit = reddit => ({
+  type: INVALIDATE_REDDIT,
+  reddit
+})
+
+export const requestPosts = reddit => ({
+  type: REQUEST_POSTS,
+  reddit
+})
+
+export const receivePosts = (reddit, json) => ({
+  type: RECEIVE_POSTS,
+  reddit,
+  posts: json.data.children.map(child => child.data),
+  receivedAt: Date.now()
+})
+
+const fetchPosts = reddit => dispatch => {
+
+  // TODO
+
+}
+
+const shouldFetchPosts = (state, reddit) => {
+  const posts = state.postsByReddit[reddit]
+  if (!posts) {
+    return true
+  }
+  if (posts.isFetching) {
+    return false
+  }
+  return posts.didInvalidate
+}
+
+export const fetchPostsIfNeeded = reddit => (dispatch, getState) => {
+  if (shouldFetchPosts(getState(), reddit)) {
+    return dispatch(fetchPosts(reddit))
+  }
+}
